17d055ec6b85751074ad00656cfbdf4f09538f7c
Steven Choo <steven@iqnomy.com>
2016-08-25 10:19:43 +0200

    * Added all coming pages to icookie

---
+++ src/views/consent.vue
@@ -0,0 +1,66 @@
+<template>
+
+</template
+
+<script>
+
+</script>
+
+<style>
+#icookieContent ._icCookieConsent {
+	margin-bottom: 20px;
+}
+
+#icookieContent ._icCookieConsent .button {
+	padding: 8px;
+	float: none;
+	position: static;
+	margin: 0;
+	display: block;
+	text-align: center;
+	margin-bottom: 10px;
+	font-weight: bold;
+	text-decoration: none;
+	border-radius: 5px;
+	-webkit-border-radius: 5px;
+	line-height: 18px;
+	color: #FFFFFF;
+	cursor: pointer;
+}
+
+#icookieContent ._icCookieConsent .button.accept-button {
+	background: #9bdc02;
+	background: -moz-linear-gradient(top, #9bdc02 0%, #57a725 100%);
+	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#9bdc02), color-stop(100%,#57a725));
+	background: -webkit-linear-gradient(top, #9bdc02 0%,#57a725 100%);
+	background: -o-linear-gradient(top, #9bdc02 0%,#57a725 100%);
+	background: -ms-linear-gradient(top, #9bdc02 0%,#57a725 100%);
+	background: linear-gradient(to bottom, #9bdc02 0%,#57a725 100%);
+	border: 1px solid rgb(113, 169, 43);
+}
+
+#icookieContent ._icCookieConsent .button.cookie-settings {
+	background-color: #4c76a9 !important;
+	background-image: -webkit-gradient(linear, left top, left bottom, from(#87b0d2), to(#4c76a9)) !important;
+	background-image: -webkit-linear-gradient(top, #87b0d2, #4c76a9) !important;
+	background-image: -moz-linear-gradient(top, #87b0d2, #4c76a9) !important;
+	background-image: -ms-linear-gradient(top, #87b0d2 0%, #4c76a9 100%) !important;
+	background-image: -o-linear-gradient(top, #87b0d2, #4c76a9) !important;
+	background-image: linear-gradient(to bottom, #87b0d2, #4c76a9) !important;
+}
+
+#icookieContent ._icCookieConsent ._icLink {
+	color: #191919;
+	font-size: 12px;
+	font-weight: normal;
+	text-decoration: underline;
+	cursor: pointer !important;
+	text-align: center;
+	display: block;
+	margin-bottom: 10px;
+}
+
+#icookieContent ._icCookieConsent ._icConsentText {
+	margin: 20px 0px;
+}
+</style>
