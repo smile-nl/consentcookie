ef5dc7583f1e14a01c6bff89f60443ffe5d31990
Steven Choo <steven@iqnomy.com>
2016-08-26 14:39:20 +0200

    * Updated build files

--- build/build-dev.js
+++ build/build-dev.js
@@ -1,65 +1,30 @@
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var config = require('../config')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing' ? require('./webpack.prod.conf') : require('./webpack.dev.conf')
+// https://github.com/shelljs/shelljs
+require('shelljs/global');
+env.NODE_ENV = 'production';
 
-// default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
-// Define HTTP proxies to your custom API backend
-// https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
+var path = require('path');
+var config = require('../config');
+var ora = require('ora');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.dev.conf');
 
-var app = express()
-var compiler = webpack(webpackConfig)
+var spinner = ora('building for testing... ');
+spinner.start();
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-	publicPath : webpackConfig.output.publicPath,
-	stats : {
+var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory);
+rm('-rf', assetsPath);
+mkdir('-p', assetsPath);
+cp('-R', 'static/', assetsPath);
+
+webpack(webpackConfig, function(err, stats) {
+	spinner.stop();
+	if (err)
+		throw err;
+	process.stdout.write(stats.toString({
 		colors : true,
-		chunks : false
-	}
-})
-
-var hotMiddleware = require('webpack-hot-middleware')(compiler)
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function(compilation) {
-	compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
-		hotMiddleware.publish({
-			action : 'reload'
-		})
-		cb()
-	})
-})
-// proxy api requests
-Object.keys(proxyTable).forEach(function(context) {
-	var options = proxyTable[context]
-	if ( typeof options === 'string') {
-		options = {
-			target : options
-		}
-	}
-	app.use(proxyMiddleware(context, options))
-})
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
-
-// serve webpack bundle output
-app.use(devMiddleware)
-
-// enable hot-reload and state-preserving
-// compilation error display
-app.use(hotMiddleware)
-
-// serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
-
-module.exports = app.listen(port, function(err) {
-	if (err) {
-		console.log(err)
-		return
-	}
-	console.log('Listening at http://localhost:' + port + '\n')
-})
+		modules : false,
+		children : false,
+		chunks : false,
+		chunkModules : false
+	}) + '\n');
+});
