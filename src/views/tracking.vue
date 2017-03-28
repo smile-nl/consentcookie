1aba62ba8d73f17f7a0c9c2f3cf78c5a3168cb60
ed <eveline@humanswitch.io>
2017-03-28 10:12:14 +0200

    IC-56 Voorkeuren - Voorkeuren screen

--- src/views/tracking.vue
+++ src/views/tracking.vue
@@ -3,129 +3,139 @@
 		<div v-if="!trackers || trackers.length == 0" class="no-content">
 			<i class="fa fa-line-chart" aria-hidden="true"></i>
 		</div>
+
 		<div class="trackers" v-if="trackers && trackers.length > 0">
-			<div class="tracker " v-for="tracker in trackers">
-				<div class="title">{{tracker.name}}</div>
-				<div class="box inline-fix" v-on:click="showDetail(tracker)">
-					<div class="logo v-centered">
-						<img :src="tracker.icon" />	
-					</div>
-					<ic-switch class="v-centered toggle"></ic-switch>
-					<div class="more inline-fix"><span class="v-centered">meer weten</span><i class="fa fa-angle-right v-centered"></i></div>
+			<ic-content-box :title="tracker.name" v-for="tracker in trackers" class="inline-fix">
+				<div class="logo v-centered">
+					<img :src="tracker.icon" />
 				</div>
-			</div>
+				<ic-switch class="v-centered toggle"></ic-switch>
+				<ic-dropdown-button :state="tracker" :iconShowInfo="'chevron-down'" :iconHideInfo="'chevron-up'" title="Meer informatie"></ic-dropdown-button>
+
+				<div class="notification" v-if="!tracker.collapsed">
+					<div class="description">{{tracker.description}}</div>
+					<div class="properties">
+						<div class="property">
+							<i :class="'fa ' + (tracker.detail.bullets.optional ? 'fa-check ' : 'fa-close')"></i>
+							<div class="text v-centered">This {{tracker.type}} is optional</div>
+						</div>
+						<div class="property">
+							<i :class="'fa ' + (tracker.detail.bullets.profiling ? 'fa-check ' : 'fa-close')"></i>
+							<div class="text v-centered">This {{tracker.type}} creates profiles</div>
+						</div>
+						<div class="property">
+							<i :class="'fa ' + (tracker.detail.bullets.crosssite ? 'fa-check ' : 'fa-close')"></i>
+							<div class="text v-centered">This {{tracker.type}} works crosssite</div>
+						</div>
+						<div class="property">
+							<i :class="'fa ' + (tracker.detail.bullets.cookie ? 'fa-check ' : 'fa-close')"></i>
+							<div class="text v-centered">This {{tracker.type}} uses analytic cookies</div>
+						</div>
+						<div class="property">
+							<i :class="'fa ' + (tracker.detail.bullets.functional ? 'fa-check ' : 'fa-close')"></i>
+							<div class="text v-centered">This {{tracker.type}} uses functional cookies</div>
+						</div>
+					</div>
+				</div>
+			</ic-content-box>
 		</div>
 	</div>
 </template>
 <script>
-	
-	// Libraries
-	var underscore = require("underscore");
-	
-	// Custom import
-	var trackers = require('assets/json/trackers.json');
-	
-	// Defaults
-	var viewTitle = "Who is tracking you?";
-	
-	// View state
-	var data = {
-		trackers : underscore.clone(trackers.trackers)
-	};
-	
-	// Public functions
-	module.exports = {
-		name:"tracking",
-		components:{
-		},
-		data : function(){
-			return data;
-		},
-		methods : {
-			showDetail : function($tracker){
-				this.$router.push({ name: 'trackingDetail',params:{tracker:$tracker}});
-			}
-		},
-		beforeMount : function(){
-			this.$store.commit('updateView',{title: viewTitle});
-		}
-	};
-	
+
+// Libraries
+var underscore = require("underscore");
+
+// Custom import
+var trackers = require('assets/json/trackers.json');
+
+// Defaults
+var viewTitle = "Voorkeuren";
+
+// View state
+var data = {
+    trackers
+};
+
+// Public functions
+module.exports = {
+    name:"tracking",
+    components:{
+    },
+	props:{
+		tracker: Object
+	},
+    data : function(){
+        return {
+            trackers : underscore.each(trackers.trackers,function(item){
+				item.collapsed = true;
+            })
+        }
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-	
+
 	@import '../assets/scss/general-variables';
 
 	#tracking{
-		min-height: 400px;
-		
-		.trackers{
-			
-			padding:20px 10px;
-			
-			.tracker{
-				
-				margin: 10px 0px 20px;
-				
-				.title{
-					margin: 10px 0px 5px 10px;
+    min-height: 400px;
+
+		.logo{
+			width: 160px;
+			padding: 15px 0px 10px 0px;
+			margin-left: 20px;
+
+			img{
+				width: 40px;
+				height: auto;
+			}
+		}
+
+		.ic-switch{
+			margin-bottom: 25px;
+			padding-right: 0px;
+		}
+
+		.ic-dropdown-button{
+			padding-left: 15px;
+		}
+
+		.description{
+			font-style: italic;
+		}
+
+		.properties {
+			margin:20px px;
+			padding: 0px 0px 0px 5px;
+
+			.property{
+
+				margin: 10px 0px;
+
+				> *{
+					display: inline;
 				}
-				
-				.box{
-					position:relative;
-					border: $ic-content-border;
-					cursor: pointer;
-					
-					&:hover{
-						background:$ic-brand-color-faded;
-						
-						.more{
-							color: $ic-color-white;
-						}
-					}
-					
-					.logo {
-						margin: 10px;
-						width: 40px;
-						height: 40px;
-						line-height: 40px;
-						
-						img{
-							width: 100%;
-							height: auto;
-						}
-					}
-					
-					.toggle{
-						margin:0px 20px;
-					}
-					
-					.more{
-					    position: absolute;
-					    height:100%;
-					    top: 0px;
-					    right: 0px;
-					    bottom: 0px;
-					    margin: auto;
-					    
-					    .fa{
-					    	
-					    }
-					    
-					    i {
-					    	margin:0px 10px;
-					    	font-size: 30px;
-						    line-height: 36px;
-						    height: 40px;
-					    }
-					}
-					
-					
-					
+
+				.fa{
+					font-size: 20px;
+					margin-right:10px;
+				}
+
+				.fa-check{
+					color:#4caf50;
+				}
+
+				.fa-close{
+					color:#ef5350;
+				}
+
+				.text{
+					width: 250px;
+					font-size: 15px;
 				}
 			}
-			
 		}
 	}
 </style>
