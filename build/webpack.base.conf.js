0df3f42183098b99b83af969d44344230f1c3db5
Steven Choo <steven@iqnomy.com>
2017-06-28 09:13:26 +0200

    http://jira.asknow.local:8080/browse/IC-141

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,73 +10,81 @@ var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
 var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
+// Use a custom config to customise the icookie build
+var customConfigPath = (process.env.configProvided ? "" : path.resolve(__dirname, './custom/icookie-custom-config.yml'));
+
 module.exports = {
-  entry: {
-    app: './src/app.js'
-  },
-  output: {
-    path: config.build.assetsRoot,
-    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-    filename: 'icookie.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.vue', '.json'],
-    fallback: [path.join(__dirname, '../node_modules')],
-    alias: {
-      'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components'),
-	  'router' : path.resolve(__dirname, '../src/router'),
-	  'views' : path.resolve(__dirname, '../src/views'),
-	  'services' : path.resolve(__dirname, '../src/services'),
-	  'plugins' : path.resolve(__dirname, '../src/plugins'),
-    }
-  },
-  resolveLoader: {
-    fallback: [path.join(__dirname, '../node_modules')]
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        include: [
-          path.join(projectRoot, 'src')
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.(png|jpe?g|gif)(\?.*)?$/,
-        loader: process.env.NODE_ENV === 'production' ? 'base64-image-loader' : "url",
-        query: {
-          limit : 1000000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot|svg)(\?.*)?$/,
-        loader: process.env.NODE_ENV === 'production' ? 'base64-font-loader' : "url",
-        query: {
-          limit : 1000000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      },
-    ]
-  },
-  vue: {
-    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 2 versions']
-      })
-    ]
-  }
+	entry: {
+		icookie: './src/app.js'
+	},
+	output: {
+		path: config.build.assetsRoot,
+		publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+		filename: '[name].js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.vue', '.json'],
+		fallback: [path.join(__dirname, '../node_modules')],
+		alias: {
+			'src': path.resolve(__dirname, '../src'),
+			'assets': path.resolve(__dirname, '../src/assets'),
+			'components': path.resolve(__dirname, '../src/components'),
+			'router': path.resolve(__dirname, '../src/router'),
+			'views': path.resolve(__dirname, '../src/views'),
+			'services': path.resolve(__dirname, '../src/services'),
+			'plugins': path.resolve(__dirname, '../src/plugins'),
+			'customConfig':path.resolve(customConfigPath),
+		}
+	},
+	resolveLoader: {
+		fallback: [path.join(__dirname, '../node_modules')]
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.vue$/,
+				loader: 'vue'
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				include: [
+					path.join(projectRoot, 'src')
+				],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.json$/,
+				loader: 'json'
+			},
+			{
+				test: /\.(yml|yaml)$/,
+				loader: 'json-loader!yaml-loader'
+			},
+			{
+				test: /\.(png|jpe?g|gif)(\?.*)?$/,
+				loader: process.env.NODE_ENV === 'production' ? 'base64-image-loader' : "url",
+				query: {
+					limit: 1000000,
+					name: utils.assetsPath('img/[name].[hash:7].[ext]')
+				}
+			},
+			{
+				test: /\.(woff|woff2|ttf|eot|svg)(\?.*)?$/,
+				loader: process.env.NODE_ENV === 'production' ? 'base64-font-loader' : "url",
+				query: {
+					limit: 1000000,
+					name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+				}
+			},
+		]
+	},
+	vue: {
+		loaders: utils.cssLoaders({sourceMap: useCssSourceMap}),
+		postcss: [
+			require('autoprefixer')({
+				browsers: ['last 2 versions']
+			})
+		]
+	}
 }
