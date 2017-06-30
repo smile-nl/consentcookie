32b8a49bcbf3abc9894f84bf87f10dd908bd8c58
Steven Choo <steven@iqnomy.com>
2017-06-30 14:17:10 +0200

    http://jira.asknow.local:8080/browse/IC-136

--- src/app.js
+++ src/app.js
@@ -2,81 +2,78 @@
 
 
 // Avoid leaking scope
-(function(){
+(function () {
 
 	// Static dependencies
 	require('font-awesome/css/font-awesome.css');
 	// require('../resources/fontello/css/animation.css')
 	// require('../resources/fontello/css/fontello.css')
 	require('./assets/scss/_icookie.scss');
-	
+
 	// Vue dependencies
 	var vue = require('vue');
 	var vueState = require('./base/configState.js');
 	var vueRouter = require('./base/configRouter.js');
-	var vuexRouterSync = require("vuex-router-sync");
-	
+
 	// Vue icookie Dependencies
 	var vueServices = require('./base/configServices.js');
-	
-	// Run init on documentready
+
+	// Run init
 	init();
-	
-	function init(){
+
+	function init() {
 		initBaseView();
 		initVue();
 	}
-	
+
 	/* Init base view*/
-	function initBaseView(){
+	function initBaseView() {
 		var icookie = document.createElement("div");
 		icookie.id = "icookie";
 		icookie.className = "icookie";
 		document.body.appendChild(icookie);
 	}
-	
-	/* Vue routing */ 
-	function initVue(){
-		
+
+	/* Vue routing */
+	function initVue() {
+
 		var store = vueState(vue);
 		var router = vueRouter(vue);
 		var services = vueServices(vue);
-		
-		
+
 		initVueResource();
 		initVueBaseComponents();
 		initVueFilters();
-		initVueRouterSync(store,router);
-			
+
 		var main = vue.extend(require('./views/main.vue'));
 		var mainInstance = new main({
 			router: router,
 			store: store,
-			services : services
+			services: services
 		}).$mount('#icookie');
 	}
-	
-	function initVueResource(){
-		
+
+	function initVueResource() {
+
 		// Init vue resource
 		var vueResource = require('vue-resource');
 		vue.use(vueResource);
 	}
-	
-	function initVueBaseComponents(){
+
+	function initVueBaseComponents() {
 		// Register the components
-		vue.component('ic-content-box',require("components/general/icContentBox.vue"));
-		vue.component('ic-button',require("components/general/icButton.vue"));
-        vue.component('ic-dropdown-button',require("components/general/icDropdownButton.vue"));
-		vue.component('ic-switch',require("components/general/icSwitch.vue"));
+		vue.component('ic-content-box', require("components/general/icContentBox.vue"));
+		vue.component('ic-button', require("components/general/icButton.vue"));
+		vue.component('ic-dropdown-button', require("components/general/icDropdownButton.vue"));
+		vue.component('ic-switch', require("components/general/icSwitch.vue"));
 	}
-	
-	function initVueFilters(){
+
+	function initVueFilters() {
 		// vue.use(vueFilter);
 	}
-	
-	function initVueRouterSync($store,$router){
-		vuexRouterSync.sync($store,$router);
+
+	function initVueRouterSync($store, $router) {
+		vuexRouterSync.sync($store, $router);
 	}
 
 })();
